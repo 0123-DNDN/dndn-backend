abc1c63c02dec47fb61244d6aff069f8f019ac8b
Kanghui <101783655+YO1231@users.noreply.github.com>
2026-09-08 15:06:46 +0900

    [#1] Chore: Spring Boot 백엔드 개발환경 구성 (#4)
    * [#1] Chore: Spring Boot 백엔드 개발환경 구성
    
    * [#1] Chore: 데이터베이스 DDL 추가

---
+++ src/main/java/com/team0123/dndn/DndnApplication.java
@@ -0,0 +1,13 @@
+package com.team0123.dndn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team0123/dndn/DndnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team0123.dndn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
